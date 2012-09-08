95bf13f1e952da012b63880140afec2dca933139
David <David@192.168.1.100>
2012-09-08 15:22:51 -0400

    Added 3-tier Packages

---
+++ src/Logic/IdleLogoutTestClass.java
@@ -0,0 +1,23 @@
+package Logic;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//CEN5076
+//package my.PSM;
+
+/**
+ *
+ * @author Kurt
+ */
+public class IdleLogoutTestClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ src/Logic/LogicFacade.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author clarkep
+ */
+public class LogicFacade {
+    
+}
