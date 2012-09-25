77b548d7aaa00ed14366d205d89c52e01111b3f4
David <David@192.168.1.106>
2012-09-25 19:38:51 -0400

    Added Test Case references

--- src/Logic/appControllerParserTest.java
+++ src/Logic/appControllerParserTest.java
@@ -31,127 +31,8 @@ public class appControllerParserTest {
 		dbUtil = null;
 		c1 = null;
 	}
-
-	@Test
-	// Test a valid format string
-	public void testTimerParser_1() {
-		
-		String hour = "11:56";
-		app1.timerParser(hour);
-		
-		assertEquals("Hour", 11, app1.getHr());
-		assertEquals("Minute",56,app1.getMin());
-		
-
-	}
 	
-	@Test
-	//Test invalid string format hh
-	public void testTimerParser_2() {
-		
-		String hour = "ss:56";
-		
-		try{
-			app1.timerParser(hour);
-		}
-		
-		catch(NumberFormatException e){
-			
-			//DEBUG System.out.println(e.toString());
-			assertEquals("Hour", 0, app1.getHr());
-			assertEquals("Minute",0,app1.getMin());
-			return;
-		}
-		
-		fail("No Exception Thrown");
-	}
-	
-	@Test
-	//Test invalid string format mm
-	public void testTimerParser_3() {
-		
-
-		String hour = "09: !the rest dont matter";
-		
-		try{
-			app1.timerParser(hour);
-		}
-		
-		catch(NumberFormatException e){
-			
-			//DEBUG System.out.println(e.toString());
-			assertEquals("Hour", 9, app1.getHr());
-			assertEquals("Minute",0,app1.getMin());
-			return;
-		}
-		
-		fail("No Exception Thrown");
-	}
-	
-	@Test
-	//Test invalid string format '+' too long
-	public void testTimerParser_4() {
-		
-		String hour = "09::07the rest dont matter";
-		
-		try{
-			app1.timerParser(hour);
-		}
-		
-		catch(NumberFormatException e){
-			
-			//DEBUG System.out.println(e.toString());
-			assertEquals("Hour", 9, app1.getHr());
-			assertEquals("Minute",0,app1.getMin());
-			return;
-		}
-		
-		fail("No Exception Thrown");
-	}
-	
-	@Test
-	//Test invalid string format '+' too short
-	public void testTimerParser_5() {
-		
-		String hour = "0907the rest dont matter";
-		
-		try{
-			app1.timerParser(hour);
-		}
-		
-		catch(NumberFormatException e){
-			
-			//DEBUG System.out.println(e.toString());
-			assertEquals("Hour", 9, app1.getHr());
-			assertEquals("Minute",0,app1.getMin());
-			return;
-		}
-		
-		fail("No Exception Thrown");
-	}
-	
-	@Test
-	//Test invalid string format
-	public void testTimerParser_6() {
-		
-String hour = "9";
-		
-		try{
-			app1.timerParser(hour);
-		}
-		
-		catch(Exception e){
-			
-			//DEBUG System.out.println(e.toString());
-			assertEquals("Hour", 0, app1.getHr());
-			assertEquals("Minute",0,app1.getMin());
-			return;
-		}
-		
-		fail("No Exception Thrown");
-	}
-
-	@Test
+	@Test //PSM014_End_Semester_Clear-UnitTest-C01
 	/* Test a valid formatted string */
 	public void testDateParser_1() {
 
@@ -164,7 +45,7 @@ String hour = "9";
 
 	}
 	
-	@Test
+	@Test //PSM014_End_Semester_Clear-UnitTest-C02
 	/* Test an invalid formatted string for 'mm'*/
 	public void testDateParser_2() {
 
@@ -189,7 +70,7 @@ String hour = "9";
 
 
 
-@Test
+@Test //PSM014_End_Semester_Clear-UnitTest-C03
 /* Test an invalid formatted string for 'dd'*/
 public void testDateParser_3() {
 
@@ -212,7 +93,7 @@ public void testDateParser_3() {
 }
 
 
-@Test
+@Test //PSM014_End_Semester_Clear-UnitTest-C04
 /* Test an invalid formatted string for 'yy'*/
 public void testDateParser_4() {
 
@@ -234,7 +115,7 @@ public void testDateParser_4() {
 	
 }
 
-@Test
+@Test //PSM014_End_Semester_Clear-UnitTest-C05
 /* Test an invalid formatted string for '+' more than 1 char*/
 public void testDateParser_5() {
 
@@ -256,7 +137,7 @@ public void testDateParser_5() {
 	
 }
 
-@Test
+@Test //PSM014_End_Semester_Clear-UnitTest-C06
 /* Test an invalid formatted string for '+' less one char*/
 public void testDateParser_6() {
 
@@ -280,4 +161,125 @@ public void testDateParser_6() {
 	
 	
 }
-}
+
+
+	@Test //PSM001_Login-UnitTest-C07
+	// Test a valid format string
+	public void testTimerParser_1() {
+		
+		String hour = "11:56";
+		app1.timerParser(hour);
+		
+		assertEquals("Hour", 11, app1.getHr());
+		assertEquals("Minute",56,app1.getMin());
+		
+
+	}
+	
+	@Test //PSM001_Login-UnitTest-C08
+	//Test invalid string format hh
+	public void testTimerParser_2() {
+		
+		String hour = "ss:56";
+		
+		try{
+			app1.timerParser(hour);
+		}
+		
+		catch(NumberFormatException e){
+			
+			//DEBUG System.out.println(e.toString());
+			assertEquals("Hour", 0, app1.getHr());
+			assertEquals("Minute",0,app1.getMin());
+			return;
+		}
+		
+		fail("No Exception Thrown");
+	}
+	
+	@Test //PSM001_Login-UnitTest-C09
+	//Test invalid string format mm
+	public void testTimerParser_3() {
+		
+
+		String hour = "09: !the rest dont matter";
+		
+		try{
+			app1.timerParser(hour);
+		}
+		
+		catch(NumberFormatException e){
+			
+			//DEBUG System.out.println(e.toString());
+			assertEquals("Hour", 9, app1.getHr());
+			assertEquals("Minute",0,app1.getMin());
+			return;
+		}
+		
+		fail("No Exception Thrown");
+	}
+	
+	@Test //PSM001_Login-UnitTest-C10
+	//Test invalid string format '+' too long
+	public void testTimerParser_4() {
+		
+		String hour = "09::07the rest dont matter";
+		
+		try{
+			app1.timerParser(hour);
+		}
+		
+		catch(NumberFormatException e){
+			
+			//DEBUG System.out.println(e.toString());
+			assertEquals("Hour", 9, app1.getHr());
+			assertEquals("Minute",0,app1.getMin());
+			return;
+		}
+		
+		fail("No Exception Thrown");
+	}
+	
+	@Test //PSM001_Login-UnitTest-C11
+	//Test invalid string format '+' too short
+	public void testTimerParser_5() {
+		
+		String hour = "0907the rest dont matter";
+		
+		try{
+			app1.timerParser(hour);
+		}
+		
+		catch(NumberFormatException e){
+			
+			//DEBUG System.out.println(e.toString());
+			assertEquals("Hour", 9, app1.getHr());
+			assertEquals("Minute",0,app1.getMin());
+			return;
+		}
+		
+		fail("No Exception Thrown");
+	}
+	
+	@Test //PSM001_Login-UnitTest-C12
+	//Test invalid string format
+	public void testTimerParser_6() {
+		
+String hour = "9";
+		
+		try{
+			app1.timerParser(hour);
+		}
+		
+		catch(Exception e){
+			
+			//DEBUG System.out.println(e.toString());
+			assertEquals("Hour", 0, app1.getHr());
+			assertEquals("Minute",0,app1.getMin());
+			return;
+		}
+		
+		fail("No Exception Thrown");
+	}
+
+	}
