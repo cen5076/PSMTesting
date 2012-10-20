9243cb0d025ff00ffb4c315747302ef24d09e550
David <David@David-UB>
2012-10-19 23:01:49 -0400

    added test suite for datastore

---
+++ src/testing/dataStoreUnitTest/AllDataStoreTests.java
@@ -0,0 +1,12 @@
+package testing.dataStoreUnitTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DBConnectionTest.class, DBConnectionTest2.class,
+		DBConnectionTest3.class })
+public class AllDataStoreTests {
+
+}
